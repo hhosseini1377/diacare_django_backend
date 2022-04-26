03a25a2e270a6793e58e56c12d4aca5cc91b0a84
hhosseini1377 <hhosseini@ce.sharif.edu>
2022-04-26 12:40:32 +0430

    cors added

--- diacare_django_backend/settings.py
+++ diacare_django_backend/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = [
     'localhost:8000'
 ]
 
+CORS_ALLOW_ALL_ORIGINS: True
+
 # Application definition
 
 
@@ -70,9 +72,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'diacare_django_backend.urls'
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
-CORS_ALLOW_CREDENTIALS = True
+
 
 TEMPLATES = [
     {
