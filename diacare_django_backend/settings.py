20cbf211f97c89592861474eb54d64060f858d63
hhosseini1377 <hhosseini@ce.sharif.edu>
2022-04-26 11:18:58 +0430

    cors headers added

--- diacare_django_backend/settings.py
+++ diacare_django_backend/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'diacare_django_backend.urls'
 
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 
