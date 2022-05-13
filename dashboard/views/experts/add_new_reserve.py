d1d3f8d78f9c451bd3606eeda1f449c85c38c444
Nima Fathi <nimafathinimafathi@gmail.com>
2022-05-14 01:52:21 +0430

    Fix: fix timezone in "add new reserve time" API in expert's dashborad

--- dashboard/views/experts/add_new_reserve.py
+++ dashboard/views/experts/add_new_reserve.py
@@ -1,19 +1,32 @@
-from diabetes_control.models import VisitTime
-from rest_framework.views import APIView
-from _helpers.custom_permisions import IsDoctor
+from datetime import datetime
 
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from _helpers.custom_permisions import IsDoctor
+from diabetes_control.models import VisitTime
 
 
 class AddNewReserveTime(APIView):
     permission_classes = [IsDoctor, ]
 
     def post(self, request):
-        start_date = request.data['start_date']
-        end_date = request.data['end_date']
+        format = "%Y-%m-%dT%H:%M:%S%z"
+        start_date = datetime.strptime(request.data['start_date'], format)
+        end_date = datetime.strptime(request.data['end_date'], format)
+        count = request.data['count']
+        chunk = (end_date - start_date) / count
+        visittime_objects = []
+        visits = []
         try:
-            VisitTime.objects.create(start_date=start_date, end_date=end_date, doctor=request.user)
+            for i in range(count):
+                start = start_date + chunk * i
+                end = start_date + chunk * (i + 1)
+                visits.append((start, end))
+                visittime_objects.append(VisitTime(start_date=start, end_date=end, doctor=request.user))
+            VisitTime.objects.bulk_create(visittime_objects)
+
             return Response({'message': 'زمان رزرو اضافه شد'}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'message': 'شما قادر به انجام این کار نیستید'}, status=status.HTTP_400_BAD_REQUEST)
