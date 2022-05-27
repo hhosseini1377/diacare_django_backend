30a52b3ccc54f61b33a899a934b42d5e379866ce
hhosseini1377 <hhosseini@ce.sharif.edu>
2022-05-27 21:22:03 +0430

    view profile now works with url

--- account/serializers/account_retrieve_serializer.py
+++ account/serializers/account_retrieve_serializer.py
@@ -7,15 +7,9 @@ from account.models import Account
 
 
 class AccountRetrieveSerializer(serializers.ModelSerializer):
-    avatar = serializers.SerializerMethodField()
 
     class Meta:
         model = Account
         fields = ['pk', 'avatar', 'email', 'is_staff', 'phone', 'first_name', 'last_name', 'is_active',
                   'optional_information']
 
-    def get_avatar(self, obj):
-        f = open(obj.avatar.path, 'rb')
-        image = File(f)
-        data = base64.b64encode(image.read())
-        return data
